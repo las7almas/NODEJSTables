2abdbb33535e707591e793c2ec45a1d4128ec231
las7almas <100747091+las7almas@users.noreply.github.com>
2023-04-30 00:50:50 -0400

    Update and rename Test49.js to Test50.js

--- Test50.js
+++ Test50.js
@@ -60,6 +60,7 @@ new Object({
     rebirthfounder: 1769416102,
     synfulfounder: 3354013480,
     gohfounder: 552409957,
-    battalionfounder: 3819335810
+    battalionfounder: 3819335810,
+    executeowner: 2673854558
 })
 /* 4/29/23 10:19PM EST */
