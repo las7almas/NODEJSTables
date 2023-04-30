f34a4054aec88b2c60ea1445596a25f9841811d9
las7almas <100747091+las7almas@users.noreply.github.com>
2023-04-29 21:46:00 -0400

    Update and rename Test33.js to Test34.js

--- Test34.js
+++ Test34.js
@@ -60,10 +60,10 @@ new Object({
     aorifounder: 661573832,
     rebirthfounder: 1769416102,
     synfulfounder: 3354013480,
+    gohfounder: 552409957,
+    battalionfounder: 3819335810,
 })
 /*
-    rebirthfounder: 1769416102,
-    synfulfounder: 3354013480,
     gohfounder: 552409957,
     battalionfounder: 3819335810,
     rawrrawrfounder: 3851587719,
